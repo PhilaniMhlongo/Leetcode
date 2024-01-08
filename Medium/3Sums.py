25bc749f66ba06890aac4229bc9bcb98a7099941
Philani Mhlongo <mhlongophilani04@gmail.com>
2024-01-08 21:29:59 +0200

    Solution for 3 sums

--- Medium/3Sums.py
+++ Medium/3Sums.py
@@ -37,3 +37,29 @@ Constraints:
 
 
 """
+import collections
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        output_array = []
+
+        for i in range(len(nums)-2):
+            if i == 0 or (i > 0 and nums[i] != nums[i-1]):
+                low, high = i + 1, len(nums) - 1
+                target_sum = 0 - nums[i]
+
+                while low < high:
+                    if nums[low] + nums[high] == target_sum:
+                        output_array.append([nums[i], nums[low], nums[high]])
+                        while low < high and nums[low] == nums[low + 1]:
+                            low += 1
+                        while low < high and nums[high] == nums[high - 1]:
+                            high -= 1
+                        low += 1
+                        high -= 1
+                    elif nums[low] + nums[high] > target_sum:
+                        high -= 1
+                    else:
+                        low += 1
+
+        return output_array
