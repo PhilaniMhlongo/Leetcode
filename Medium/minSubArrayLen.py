6ede54636bd48b522c8bf9ac32ad96d34c6a35a2
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-01-10 19:57:13 +0200

    Solution minSubArrayLen.py

--- Medium/minSubArrayLen.py
+++ Medium/minSubArrayLen.py
@@ -32,3 +32,19 @@ Constraints:
  
 Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
 """
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        result = float('inf')
+        left = 0
+        val_sum = 0
+
+        for i in range(len(nums)):
+            val_sum += nums[i]
+            while val_sum >= target:
+                result = min(result, i + 1 - left)
+                val_sum -= nums[left]
+                left += 1
+
+        return result if result != float('inf') else 0
+
