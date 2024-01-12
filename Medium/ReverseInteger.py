a085fda4881d25770bcb3fd53046cf29e73b6cf9
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-01-12 21:04:53 +0200

    Update ReverseInteger.py

--- Medium/ReverseInteger.py
+++ Medium/ReverseInteger.py
@@ -28,3 +28,26 @@ Constraints:
 
 
 """
+
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+    
+        sign = 1 if x >= 0 else -1
+        x = abs(x)
+
+        reversed_num = 0
+
+        while x > 0:
+            digit = x % 10
+            
+            if reversed_num > (INT_MAX - digit) // 10:
+                return 0
+            reversed_num = reversed_num * 10 + digit
+            x = x // 10
+
+        return sign * reversed_num
+
