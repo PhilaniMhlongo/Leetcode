b2052b599ad5e6aafde4d22e449852bc38216fd6
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-01-16 20:56:46 +0200

    Update containsDuplicate.py

--- Easy/containsDuplicate.py
+++ Easy/containsDuplicate.py
@@ -27,3 +27,11 @@ Constraints:
 
 
 """
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
+        
