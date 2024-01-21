863ef3780f5e2caf32772361764e3b93d0d47f8b
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-01-21 18:58:23 +0200

    Update findIntersectionValues.py

--- Easy/findIntersectionValues.py
+++ Easy/findIntersectionValues.py
@@ -35,3 +35,13 @@ Constraints:
 
 
 """
+
+class Solution:
+    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        common_in_nums1 = sum(1 for num in nums1 if num in set2)
+        common_in_nums2 = sum(1 for num in nums2 if num in set1)
+
+        return [common_in_nums1, common_in_nums2]
