447f193f779d84daec8ce8829ea8310f35a87a96
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-01-25 16:38:59 +0200

    Update searchInsert.py

--- Easy/searchInsert.py
+++ Easy/searchInsert.py
@@ -32,4 +32,21 @@ Constraints:
 
 
 """
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
 
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        # At this point, left is the position where the target should be inserted
+        return left
+
+        
