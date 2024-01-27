c85737065c8ea8d609c26b7b23850fbbdbd59be7
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-01-27 23:55:38 +0200

    Solution for mergeTwoLists.py

--- Easy/mergeTwoLists.py
+++ Easy/mergeTwoLists.py
@@ -33,3 +33,50 @@ Constraints:
 
 
 """
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        current = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+
+        if list1:
+            current.next = list1
+        else:
+            current.next = list2
+
+        return dummy.next
+
+
+    def listToLinkedList(lst):
+        dummy = ListNode()
+        current = dummy
+        for val in lst:
+            current.next = ListNode(val)
+            current = current.next
+        return dummy.next
+
+
+    def linkedListToList(head):
+        result = []
+        current = head
+        while current:
+            result.append(current.val)
+            current = current.next
+        return result
+            
+
