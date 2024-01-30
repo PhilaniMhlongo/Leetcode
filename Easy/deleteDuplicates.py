e88f2e4bdad58dfe688dfe446bffc7afd598dec8
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-01-30 19:22:45 +0200

    Solution of deleteDuplicates.py

--- Easy/deleteDuplicates.py
+++ Easy/deleteDuplicates.py
@@ -22,3 +22,35 @@ Constraints:
     The list is guaranteed to be sorted in ascending order
 """
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
+        
+
+    def listToLinkedList(lst):
+        dummy = ListNode()
+        current = dummy
+        for val in lst:
+            current.next = ListNode(val)
+            current = current.next
+        return dummy.next
+
+    def linkedListToList(head):
+        result = []
+        current = head
+        while current:
+            result.append(current.val)
+            current = current.next
+        return result
