d77a1a66f4324016e5c0e367b1d55ed4b1c7a288
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-01-31 17:15:40 +0200

    Solution of pascal_triangle.py

--- Easy/pascal_triangle.py
+++ Easy/pascal_triangle.py
@@ -25,3 +25,24 @@ Constraints:
 
 
 """
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+
+        triangle = [[1]]
+
+        for i in range(1, numRows):
+            prev_row = triangle[i - 1]
+            current_row = [1]
+
+            for j in range(1, i):
+                current_row.append(prev_row[j - 1] + prev_row[j])
+
+            current_row.append(1)
+            triangle.append(current_row)
+
+        return triangle
+
+        
