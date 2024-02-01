d99c24e230b83259f35167cf2555c384ea227c54
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-01 17:06:10 +0200

    Solution of  validPalindrome.py

--- Easy/validPalindrome.py
+++ Easy/validPalindrome.py
@@ -33,3 +33,9 @@ Constraints:
 
 
 """
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        return s == s[::-1]
+        
