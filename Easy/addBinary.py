98e1848610ecbaa977e02929a06694e9df785817
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-02 17:41:19 +0200

    Solution of  addBinary.py

--- Easy/addBinary.py
+++ Easy/addBinary.py
@@ -24,3 +24,21 @@ Constraints:
 
 
 """
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = []
+        carry = 0
+
+        # Pad the shorter binary string with leading zeros
+        a, b = a.zfill(max(len(a), len(b))), b.zfill(max(len(a), len(b)))
+
+        for i in range(len(a) - 1, -1, -1):
+            bit_sum = int(a[i]) + int(b[i]) + carry
+            result.append(str(bit_sum % 2))
+            carry = bit_sum // 2
+
+        if carry:
+            result.append(str(carry))
+
+        return ''.join(result[::-1])
+        
