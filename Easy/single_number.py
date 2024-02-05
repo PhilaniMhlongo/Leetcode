54a20f08c994306d716cf632d848311cadbcd8b5
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-05 17:21:09 +0200

    Solution single_number.py

--- Easy/single_number.py
+++ Easy/single_number.py
@@ -31,3 +31,9 @@ Constraints:
 
 
 """
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
