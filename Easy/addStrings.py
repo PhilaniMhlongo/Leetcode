c264a0ce23c23b4ef477b83e6840a601b7edece4
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-06 17:22:40 +0200

    Solution of addStrings.py

--- Easy/addStrings.py
+++ Easy/addStrings.py
@@ -31,3 +31,25 @@ Constraints:
 
 
 """
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        i = len(num1) - 1
+        j = len(num2) - 1
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+            digit_sum = carry
+            if i >= 0:
+                digit_sum += int(num1[i])
+                i -= 1
+            if j >= 0:
+                digit_sum += int(num2[j])
+                j -= 1
+            carry = digit_sum // 10
+            digit_sum %= 10
+            result.append(str(digit_sum))
+
+        return ''.join(result[::-1])
+            
+
