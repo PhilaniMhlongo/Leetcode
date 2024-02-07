6cec47223538b24598e4025c034fe28e8b409df9
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-07 17:29:42 +0200

    Solution of  goat_latin.py

--- Easy/goat_latin.py
+++ Easy/goat_latin.py
@@ -36,3 +36,19 @@ Constraints:
 
 
 """
+
+
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        words = sentence.split()
+        result = []
+        
+        for i, word in enumerate(words, start=1):
+            if word[0].lower() in vowels:
+                result.append(word + 'ma' + 'a' * i)
+            else:
+                result.append(word[1:] + word[0] + 'ma' + 'a' * i)
+        
+        return ' '.join(result)
+        
