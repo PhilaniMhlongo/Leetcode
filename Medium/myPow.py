27dda93117cbcad3bebb90067189905f8d0fec53
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-08 16:56:33 +0200

    Solution of  myPow.py

--- Medium/myPow.py
+++ Medium/myPow.py
@@ -30,3 +30,19 @@ Constraints:
 
 
 """
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        result = 1
+        while n:
+            if n % 2:
+                result *= x
+            x *= x
+            n //= 2
+
+        return result
+        
+
