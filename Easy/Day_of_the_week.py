dd36493199669e06cd1777eb9d404656ead03876
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-10 16:32:43 +0200

    Solution of Day_of_the_week.py

--- Easy/Day_of_the_week.py
+++ Easy/Day_of_the_week.py
@@ -31,3 +31,20 @@ Constraints:
 
 
 """
+class Solution:
+    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+        days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
+        if month < 3:
+            month += 12
+            year -= 1
+        
+        q = day
+        m = month
+        K = year % 100
+        J = year // 100
+        
+        h = (q + ((13 * (m + 1)) // 5) + K + (K // 4) + (J // 4) - (2 * J)) % 7
+        
+        return days_of_week[(h + 6) % 7] 
+        
