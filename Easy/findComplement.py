4b9e3d7e0117cc0b87edb5c26e2a2185cba00fcc
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-12 17:29:31 +0200

    Update findComplement.py

--- Easy/findComplement.py
+++ Easy/findComplement.py
@@ -27,3 +27,16 @@ Constraints:
 
 
 """
+class Solution:
+    def findComplement(self, num: int) -> int:
+        bit_length = num.bit_length()
+    
+        
+        bitmask = (1 << bit_length) - 1
+        
+       
+        complement = num ^ bitmask
+        
+        return complement
+        
+
