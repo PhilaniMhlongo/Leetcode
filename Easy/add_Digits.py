5d2036b579f4ccf492ad6aeb9f2ae9b3cf1b5d89
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-13 23:48:19 +0200

    Solution of add_Digits.py

--- Easy/add_Digits.py
+++ Easy/add_Digits.py
@@ -26,3 +26,11 @@ Constraints:
  
 
 """
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >= 10:
+            num_str = str(num)
+            num = sum(int(digit) for digit in num_str)
+        
+        return num
+        
