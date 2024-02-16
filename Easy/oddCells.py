8b0919b9e5d7c16beda15ae4ebccc3ff0f944aff
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-16 15:56:53 +0200

    Solution of oddCells.py

--- Easy/oddCells.py
+++ Easy/oddCells.py
@@ -39,3 +39,21 @@ Follow up: Could you solve this in O(n + m + indices.length) time with only O(n 
 
 
 """
+
+class Solution:
+    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+        rows = [0] * m
+        cols = [0] * n
+        
+        for r, c in indices:
+            rows[r] += 1
+            cols[c] += 1
+        
+        odd_count = 0
+        for i in range(m):
+            for j in range(n):
+                if (rows[i] + cols[j]) % 2 != 0:
+                    odd_count += 1
+                    
+        return odd_count
+            
