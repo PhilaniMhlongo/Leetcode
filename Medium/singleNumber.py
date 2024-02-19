6f2dfbb8a6068977949426d63cf413fb94eb43e5
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-19 16:52:09 +0200

    Solution of singleNumber.py

--- Medium/singleNumber.py
+++ Medium/singleNumber.py
@@ -31,3 +31,25 @@ Constraints:
 
 
 """
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        
+        xor_result = 0
+        for num in nums:
+            xor_result ^= num
+        
+        
+        rightmost_set_bit = 1
+        while (rightmost_set_bit & xor_result) == 0:
+            rightmost_set_bit <<= 1
+        
+    
+        group1, group2 = 0, 0
+        for num in nums:
+            if num & rightmost_set_bit:
+                group1 ^= num
+            else:
+                group2 ^= num
+        
+        return [group1, group2]
+            
