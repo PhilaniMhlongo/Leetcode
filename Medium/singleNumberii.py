7267c1ca616b1988ecaefb5cbef24ba6194ccc4d
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-23 15:31:28 +0200

    Solution of singleNumberii.py

--- Medium/singleNumberii.py
+++ Medium/singleNumberii.py
@@ -26,3 +26,16 @@ Constraints:
 
 
 """
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for i in range(32):
+            count = 0
+            for num in nums:
+                count += (num >> i) & 1
+            result |= (count % 3) << i
+        
+        return result if result < 2**31 else result - 2**32
+
+        
