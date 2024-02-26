4b23ac14c7da366b45c68bdaaaa98fecad0e3613
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-02-26 17:06:27 +0200

    Update hasCycle.py

--- Easy/hasCycle.py
+++ Easy/hasCycle.py
@@ -39,3 +39,26 @@ Follow up: Can you solve it using O(1) (i.e. constant) memory?
 
 
 """
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+
+        slow = head
+        fast = head.next
+
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+
+        return True
+        
