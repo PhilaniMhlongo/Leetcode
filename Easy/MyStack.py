9d7d73d460d9671849683e17f45631db7c46c39a
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-03-05 16:08:54 +0200

    Solution of MyStack.py

--- Easy/MyStack.py
+++ Easy/MyStack.py
@@ -36,3 +36,33 @@ Constraints:
 At most 100 calls will be made to push, pop, top, and empty.
 All the calls to pop and top are valid.
 """
+
+class MyStack:
+
+    def __init__(self):
+        
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        top_element = self.q1.popleft()
+        self.q1, self.q2 = self.q2, self.q1
+        return top_element
+
+    def top(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        top_element = self.q1.popleft()
+        self.q2.append(top_element)
+        self.q1, self.q2 = self.q2, self.q1
+        return top_element
+
+    def empty(self) -> bool:
+        return not self.q1
+        
+
