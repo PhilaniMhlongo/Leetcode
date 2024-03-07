54db261cee3d8cb2ea42d71f029480682062b239
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-03-07 06:15:03 +0200

    Solution of getIntersectionNode.py

--- Easy/getIntersectionNode.py
+++ Easy/getIntersectionNode.py
@@ -59,3 +59,37 @@ Constraints:
  
 Follow up: Could you write a solution that runs in O(m + n) time and use only O(1) memory?
 """
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        def getLength(node):
+            length = 0
+            while node:
+                length += 1
+                node = node.next
+            return length
+
+        
+        lenA = getLength(headA)
+        lenB = getLength(headB)
+
+        
+        while lenA > lenB:
+            headA = headA.next
+            lenA -= 1
+        while lenB > lenA:
+            headB = headB.next
+            lenB -= 1
+
+        
+        while headA != headB:
+            headA = headA.next
+            headB = headB.next
+
+        return headA
