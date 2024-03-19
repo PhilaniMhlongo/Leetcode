a6745507fc60d5a433c4b5b098734aed4c70e18b
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-03-19 16:47:01 +0200

    Solution of  find132pattern.py

--- Easy/find132pattern.py
+++ Easy/find132pattern.py
@@ -34,3 +34,20 @@ Constraints:
 
 
 """
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+
+        stack = []
+        s3 = float('-inf')
+        
+        for num in reversed(nums):
+            if num < s3:
+                return True
+            while stack and stack[-1] < num:
+                s3 = stack.pop()
+            stack.append(num)
+        
+        return False
+
+        
