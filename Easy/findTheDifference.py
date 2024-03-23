7e71e3d1c751f2bb2b342aca57b4932c7f5c939b
PhilaniMhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-03-23 02:56:13 +0200

    Solution of findTheDifference.py

--- Easy/findTheDifference.py
+++ Easy/findTheDifference.py
@@ -28,3 +28,16 @@ Constraints:
 
 
 """
+from collections import Counter
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s_count = Counter(s)
+        t_count = Counter(t)
+
+        for char in t_count:
+            if t_count[char] > s_count[char]:
+                return char
+
+        return ""
+        
+
