49dbec9e0a822df1fe8e26849d4ac0aa6b72a04e
Philani Mhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-03-27 08:44:28 +0200

    Solution of findSubsequence.py

--- Medium/findSubsequence.py
+++ Medium/findSubsequence.py
@@ -22,3 +22,26 @@ Constraints:
 
 
 """
+
+
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, path):
+            if len(path) > 1:
+                res.append(path[:])
+            if start == len(nums):
+                return
+            used = set()
+            for i in range(start, len(nums)):
+                if nums[i] in used:
+                    continue
+                if not path or nums[i] >= path[-1]:
+                    used.add(nums[i])
+                    path.append(nums[i])
+                    backtrack(i + 1, path)
+                    path.pop()
+
+        res = []
+        backtrack(0, [])
+        return res
+            
